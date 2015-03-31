1fbd2d6f6b281de9ac5052951c57f8c095ba6b05
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-04-01 00:04:36 +1100

    make ticks appear on hover for dynamically added items

--- js/app.js
+++ js/app.js
@@ -12,57 +12,43 @@ $(document).ready(function() {
 
 //Showing green tick on hover for 'pending' items
 
-	//the following doesn't work on items added dynamically
-
-	$("li.pending")
-		.hover(
-			function() {
-			$(this).append($('<span class="tick"> </span>'));
-		}, function() {
-			$(this).find('span:last').remove();
-		})
-
-	//trying other option with .on() but this doesn't work
-
-//	$(document)
-//		.on("hover", "li.pending", function() {
-//			$("li.pending").append('<span class="tick"> </span>');
-//		}, function() {
-//			$("li.pending").find('span:last').remove();
-	//	})
-
-
-//Showing red cross on hover for 'done' items
-
-	//the following doesn't work on items added dynamically
-
-	$("li.done")
-		.hover(
-			function() {
-			$(this).append($('<span class="delete"> </span>'));
-		}, function() {
-			$(this).find('span:last').remove();
-		})
+	$("ul.list-items")
+		.on("mouseenter", "li.pending", function() {
+                $(this).append('<span class="tick"> </span>');
+            })
+		.on("mouseleave", "li.pending", function() {
+                $(this).find('span.tick:last').remove();
+            })
 
 //Changing an item from 'pending' to 'done' on clicking a pending item
-
-	//this changes the styling but doesn't show the appropriate icon ('span.delete')
 
 	$("ul.list-items")
 		.on("click", "li.pending", function() {
   			$(this).removeClass('pending').addClass('done');
+  			$(this).find('span.tick').remove();
 		})
+
+//Showing red cross on hover for 'done' items
+
+	$("ul.list-items")
+		.on("mouseenter", "li.done", function() {
+                $(this).append('<span class="delete"> </span>');
+            })
+		.on("mouseleave", "li.done", function() {
+                $(this).find('span.delete:last').remove();
+            })
 
 //Removing an item from the list on clicking the red cross
 
-	//doesn't work
+	//the following doesn't work on items added dynamically
 
 	$("li.done")
 		.on("click", "span.delete", function() {
-  			$(this).remove();
+  			$("li.done:last").remove();
 	})
 
 //Order items so 'done' items always sit below all 'pending' items.
 
 
 })
+
